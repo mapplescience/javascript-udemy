e7ef8a20f0ec549a8fce9fe6c49bcffb526b8bf0
Yusuke Tsuji <tsjysk@gmail.com>
2023-03-19 23:02:10 +0900

    02の途中

---
+++ 01_quote-generator/script.js
@@ -0,0 +1,70 @@
+// https://jacintodesign.github.io/quotes-api/data/quotes.json
+
+const quoteContainer = document.getElementById("quote-container")
+const quoteText = document.getElementById("quote")
+const authorText = document.getElementById("author")
+const twitterBtn = document.getElementById("twitter")
+const newQuoteBtn = document.getElementById("new-quote")
+const loader = document.getElementById("loader")
+
+let apiQuotes = []
+
+function showLoadingSpinner() {
+    loader.hidden = false
+    quoteContainer.hidden = true
+}
+
+function removeLoadingSpinner(){
+    quoteContainer.hidden = false
+    loader.hidden = true
+}
+
+
+// Show New Quote
+function newQuote(){
+    showLoadingSpinner()
+    // Pick a random quote from apiQuote array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+
+    // Check if Author field is blank and replace it with "unknown"
+    if(!quote.author){
+        authorText.textContent = "Unknown"
+    } else {
+        authorText.textContent = quote.author
+    }
+
+    // Check Quote length to determine styling
+    if(quote.text.length > 50){
+        quoteText.classList.add("long-quote")
+    } else {
+        quoteText.classList.remove("long-quote")
+    }
+    // Set Quote, Hide Loader
+    quoteText.textContent = quote.text
+    removeLoadingSpinner()
+}
+
+// Get Quotes From API
+async function getQuotes(){
+    showLoadingSpinner()
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json'
+    try{
+        const response = await fetch(apiUrl)
+        apiQuotes = await response.json()
+        newQuote()
+    }catch(error){
+        // Catch Error Here
+    }
+}
+
+// Tweet Quote
+function tweetQuote(){
+    const twitterUlr = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`
+    window.open(twitterUlr, '_blank')
+}
+
+// Event Listners
+newQuoteBtn.addEventListener('click', newQuote)
+twitterBtn.addEventListener('click', tweetQuote)
+
+getQuotes()
